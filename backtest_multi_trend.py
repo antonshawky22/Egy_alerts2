f88870d85f3583043730a5e7864d962a145c9710
antonshawky22 <anton.shawky22@gmail.com>
2026-09-15 07:09:50 +0300

    Change EMA100_PERIOD from 60 to 40

--- backtest_multi_trend.py
+++ backtest_multi_trend.py
@@ -26,7 +26,7 @@ INITIAL_CAPITAL = 100000.0
 # ============================================================
 
 RSI_PERIOD = 14
-EMA100_PERIOD = 60
+EMA100_PERIOD = 40
 RUNUP_LOOKBACK = 160
 MAX_RUNUP_PERCENT = 80
 MAX_GAP_DOWN_PERCENT = -5
